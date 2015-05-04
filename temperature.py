97071e3ff0aeb2b2e1275a40832c0c9fcb146f9d
notnew <fake@fake.com>
2015-05-04 14:23:07 -0500

    read temperature of a ds18b20 sensor
    convert the reading to celsius and fahrenheit

---
+++ temperature.py
@@ -0,0 +1,44 @@
+import os
+
+class Sensor():
+    # static method and data
+    base_device_path = "/sys/bus/w1/devices/"
+    def list_devices():
+        devices = os.listdir(Sensor.base_device_path)
+        return [d for d in devices if not d.startswith("w1_bus_master")]
+
+    def __init__(self, id=None):
+        self.id = id or Sensor.list_devices()[0]
+        self.path = os.path.join(Sensor.base_device_path, self.id, 'w1_slave')
+        self.file = open(self.path)
+
+        self.celsius = "unset"
+        self.fahrenheit = "unset"
+
+    def get_temp(self):
+        self.file.seek(0)
+        self.raw_data = self.file.read()
+
+        # get last column of second line (format is t=000000)
+        raw = self.raw_data.splitlines()[1]
+        raw = raw.split(" ")[-1]
+        assert(raw.startswith("t="))
+        raw = raw[2:]
+        raw = int(raw)
+
+        celsius = raw / 1000.0
+        fahrenheit = celsius * 9/5. + 32
+
+        self.raw = raw
+        self.celsius = celsius
+        self.fahrenheit = fahrenheit
+
+    def __str__(self):
+        return "Fahrenheit: {}, Celsius: {}".format(self.fahrenheit, self.celsius)
+
+if __name__ == "__main__":
+    s = Sensor();
+    print(s)
+    for i in range(5):
+        s.get_temp()
+        print(s)
